11f9860b468088a98b97298369717b3b5d2bb567
Mikalai Hil <mikalai.hil@multiparts.net>
2021-03-15 20:21:43 +0300

    Update .gitignore

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const app = express();
 
-app.set('port', 3000);
+const config = require('./config');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.set('port', config.port);
+app.get('/summary', (req, res) => {
+    res.send('Hello World!')
 });
 
-app.use(function (req, res) {
-    res.sendStatus(404);
-    res.send('Page Not Found');
-});
+try {
+
+    app.listen(app.get('port'), () => {
+        console.log(`Example app listening at http://localhost:${config.port}`)
+        console.log(config);
+    });
+} catch (e) {
+    console.log(e);
+}
+
+
+//const express = require('express');
+//const app = express();
+//const config = require('config');
+
+//app.set('port', config.port);
+
+//console.log(config.port);
+//app.get('/summary', (req, res) => {
+//    res.send('Hello World!')
+//});
+
+//app.listen(app.get('port'), () => {
+//    console.log(`Example app listening at http://localhost:${config.port}`);
+//});
